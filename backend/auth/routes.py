fcd8a7b8d16f6c61702a0bfadae711228ca36c35
godwintrav <godwintrav@gmail.com>
2025-02-14 14:30:51 +0100

    fix: forgot to add async keyword

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -10,14 +10,14 @@ from database import get_session
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/register", response_model=TokenResponse, status_code=201)
-def register(user_data: UserCreate, session: Session = Depends(get_session)) -> TokenResponse:
+async def register(user_data: UserCreate, session: Session = Depends(get_session)) -> TokenResponse:
     return register_user(user_data, session=session)
 
 @router.post("/login", response_model=TokenResponse, status_code=200)
-def login(user_data: UserLogin, session: Session = Depends(get_session)):
+async def login(user_data: UserLogin, session: Session = Depends(get_session)):
     return login_user(user_data, session=session)
 
 @router.post("/logout", status_code=200)
-def logout(token: str, user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+async def logout(token: str, user: User = Depends(get_current_user), session: Session = Depends(get_session)):
     logout_user(token, session=session)
     return {"message": "User logged out successfully"}
