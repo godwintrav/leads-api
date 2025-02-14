d7e41d9af4022d126fce9ed7fa4ad7ccd3f8780e
godwintrav <godwintrav@gmail.com>
2025-02-14 10:30:07 +0100

    chore: added db configuration

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from database import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
 
 app = FastAPI()
 
