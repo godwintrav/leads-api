87dfba3b94552bbf605427e7d2a800fc74c54fb6
godwintrav <godwintrav@gmail.com>
2025-02-14 11:57:49 +0100

    chore: updated requirements.txt

--- backend/auth/auth.util.py
+++ backend/auth/auth.util.py
@@ -2,6 +2,9 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import jwt
 from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,3 +19,4 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     expire = datetime.now() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
