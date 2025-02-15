e721e1d3e4224880adb8e111e383c9a63c786810
godwintrav <godwintrav@gmail.com>
2025-02-15 21:22:20 +0100

    feat: add export button and hook for exporting leads data

---
+++ frontend/hooks/queries/use-export-leads.tsx
@@ -0,0 +1,12 @@
+import { queryKeys } from "@/constants/query-keys";
+import { exportLeads } from "@/services/api/lead";
+
+import { useQuery } from "@tanstack/react-query";
+
+export const useExportLeads = (enabled: boolean) => {
+  return useQuery({
+    queryFn: exportLeads,
+    queryKey: [queryKeys.leads],
+    enabled,
+  });
+};
